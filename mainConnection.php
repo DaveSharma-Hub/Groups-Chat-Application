a44bce512f475373623782c87c3cb595e0722e11
Dave Sharma <81478885+DaveSharma-Hub@users.noreply.github.com>
2022-01-01 19:09:46 -0700

    Update mainConnection.php

--- mainConnection.php
+++ mainConnection.php
@@ -1,5 +1,9 @@
 <?php
-    $connection = new mysqli("localhost","dave(2)","ensf409","group_chat");
+    $username="";
+    $password="";
+    $schema="";
+
+    $connection = new mysqli("localhost",$username,$password,$schema);
     if($connection->connect_error){
         die("Failed to connect: ".$connection->connect_error);
     }
